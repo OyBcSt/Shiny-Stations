c026cbc151df4b0cbedcc965e4d552a54ef8ee16
Lisa L. Lowe <lllowe@ncsu.edu>
2022-07-21 00:23:04 -0400

    first commit

---
+++ app.R
@@ -0,0 +1,111 @@
+# packages
+library(shiny)
+library(leaflet)
+
+r_colors <- rgb(t(col2rgb(colors()) / 255))
+names(r_colors) <- colors()
+load("/Users/lllowe/shiny-examples/david_google/disl_temp_salt.RData")
+
+# make df for leaflet
+mapDF <- data.frame(
+  location = c("Dauphin Island", "I7-F", "A7-F"),
+  lat = c(30.25125, 30.58258333, 30.17156667),
+  lng = c(-88.07783333, -88.06091667, -88.05013333),
+  hrefValue = c("disl", "two", "three")
+)
+
+
+# ui
+ui <- tagList(
+  # link js
+  tags$head(tags$link(includeScript("func.js"))),
+  tags$head(tags$style("a{cursor:pointer;}")),
+  # UI
+  navbarPage(
+    
+    title = "OyBcSt",
+    tabPanel("Home", 
+             value ="home",
+             titlePanel("Mobile Bay Stations"),
+             h4("Data available for each station"),
+             leafletOutput("map")
+    ),
+    tabPanel("Dauphin Island", 
+             value="disl",
+             titlePanel("Dauphin Island"),
+             #p("...some text here..."),
+             #helpText("Would you like to go back? If so click ", 
+             #         HTML("<a onclick=","customHref('home')" ,">",
+             #              "here","</a>")
+    radioButtons("timeplot", "Values:",
+                 c("Temperature"="temp", "Salinity"="salt")),
+    sliderInput("timeRange", label = "Time range",
+                min = as.POSIXct("2019-01-01 00:00:00"),
+                max = as.POSIXct("2019-12-31 15:00:00"),
+                value = c(as.POSIXct("2019-01-01 00:00:00"),
+                          as.POSIXct("2019-12-31 15:00:00"))),
+    plotOutput("timeplot"),
+    ),
+    tabPanel("I7-F",
+             value="two",
+             titlePanel("I7-F")
+             #p("...some text here..."),
+             #helpText("Would you like to go back? If so click ", 
+             #         HTML("<a onclick=","customHref('home')" ,">",
+             #              "here","</a>"))
+             ),
+    tabPanel("A7-F",
+             value="three",
+             titlePanel("A7-F")
+             #p("...some text here..."),
+             #helpText("Would you like to go back? If so click ", 
+             #         HTML("<a onclick=","customHref('home')" ,">",
+             #              "here","</a>")))
+  )
+)
+)
+
+# server
+server <- function(input, output){
+  
+  # make map
+  output$map <- renderLeaflet({
+   
+    leaflet(data = mapDF) %>%
+      setView(
+        lat = 30.50355, 
+        lng = -87.98733,
+        zoom = 9) %>%
+      addTiles() %>%
+      addMarkers(lng = ~lng , lat = ~lat,
+                 popup = paste0(
+                   "<a onclick=","customHref('",mapDF$hrefValue,"')>",
+                   mapDF$location,
+                   "</a>"
+                 ))
+  })
+  
+  output$timeplot <- renderPlot({
+     if(input$timeplot=="temp"){
+       y_var <- disl_temp_salt$Temperature
+       y_label <- "Temperature"
+     }
+    if(input$timeplot=="salt"){
+      y_var <- disl_temp_salt$Salinity
+      y_label <- "Salinity"
+    }
+    output$from <- renderText(input$timeRange[1]);
+    output$to <- renderText(input$timeRange[2]);
+    xlabel=paste("Time: ",as.Date(input$timeRange[1])," until ",as.Date(input$timeRange[2]))
+    #xlabel="Time"
+    plot(disl_temp_salt$Time,y_var,xlab=xlabel,ylab=y_label,pch=20,xlim=c(input$timeRange[1],input$timeRange[2]),xaxt="n")
+    axis.POSIXct(1, input$Time,format="%b %d")
+  })
+  
+}
+
+
+# launch
+shinyApp(ui, server)
+
+
