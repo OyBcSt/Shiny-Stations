10f2a4ffb6f1d3e82aaa515f393a148b54eb4ffe
Lisa L. Lowe <lllowe@ncsu.edu>
2022-07-22 10:04:39 -0400

    Turn Off Extra

--- app.R
+++ app.R
@@ -1,15 +1,16 @@
 # packages
 library(shiny)
 library(leaflet)
+options( warn = -1 )
 
 load("./disl_temp_salt.RData")
 
 # make df for leaflet
 mapDF <- data.frame(
-  location = c("Dauphin Island", "I7-F", "A7-F"),
-  lat = c(30.25125, 30.58258333, 30.17156667),
-  lng = c(-88.07783333, -88.06091667, -88.05013333),
-  hrefValue = c("disl", "two", "three")
+  location = c("Dauphin Island"),
+  lat = c(30.25125),
+  lng = c(-88.07783333),
+  hrefValue = c("disl")
 )
 
 
@@ -53,22 +54,6 @@ ui <- fluidPage(
         plotOutput("timeplot")
   ),
   
-        tabPanel("I7-F",
-             value="two",
-             titlePanel("I7-F")
-             #p("...some text here..."),
-             #helpText("Would you like to go back? If so click ", 
-             #         HTML("<a onclick=","customHref('home')" ,">",
-             #              "here","</a>"))
-             ),
-    tabPanel("A7-F",
-             value="three",
-             titlePanel("A7-F")
-             #p("...some text here..."),
-             #helpText("Would you like to go back? If so click ", 
-             #         HTML("<a onclick=","customHref('home')" ,">",
-             #              "here","</a>")))
-  ),
   tabPanel("All Stations",value="flask",titlePanel("All Stations"),
            HTML('<iframe width="560" height="315" src="https://flask.oybcst.tk" frameborder="0" allowfullscreen
       style="position:absolute;top:90;left:0;width:100%;height:100%;"></iframe>'))
@@ -107,7 +92,7 @@ server <- function(input, output){
      }
     if(input$timeplot=="both"){
       y2_var <- disl_temp_salt$Salinity
-      y2_label <- "Salinity"
+      y_label <- "Temperature (\u00B0C), Salinity"
       y2_letter <- "S"
     }
     
